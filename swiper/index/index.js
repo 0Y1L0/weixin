6559b947b6bd36f61f68cbb00546de55f3b6d208
0Y1L0 <3056174113@qq.com>
2020-04-04 13:20:30 +0800

    add: swiper

--- swiper/index/index.js
+++ swiper/index/index.js
@@ -3,34 +3,36 @@ Page({
     background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     indicatorDots: true,
     vertical: false,
-    autoplay: false,
+    autoplay: true,
     circular: false,
     interval: 2000,
     duration: 500,
     previousMargin: 0,
-    nextMargin: 0
+    nextMargin: 0,
+    indicatorColor: 'rgba(0, 0, 0, .6)',
+    indicatorActiveColor: 'rgba(255, 255, 255, .5)'
   },
+
   changeProperty: function (e) {
-    var propertyName = e.currentTarget.dataset.propertyName
-    var newData = {}
+    const propertyName = e.target.dataset.propertyName
+    const newData = {}
     newData[propertyName] = e.detail.value
     this.setData(newData)
+    console.log(newData)
   },
+
   changeIndicatorDots: function (e) {
     this.setData({
       indicatorDots: !this.data.indicatorDots
     })
   },
-  changeAutoplay: function (e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
+
   intervalChange: function (e) {
     this.setData({
       interval: e.detail.value
     })
   },
+
   durationChange: function (e) {
     this.setData({
       duration: e.detail.value
