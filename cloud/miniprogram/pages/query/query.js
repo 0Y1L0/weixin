cdbf2ed0ff36a9707bc4429a18882cf2431ba147
0Y1L0 <3056174113@qq.com>
2020-04-08 20:47:44 +0800

    临时文件路径

--- cloud/miniprogram/pages/query/query.js
+++ cloud/miniprogram/pages/query/query.js
@@ -16,7 +16,16 @@ Page({
       count: _.in([1,3,4])
     })
     .get().then(console.log)
-  }
+  },
+
+	queryUrl: function () {
+		wx.cloud.getTempFileURL({
+			fileList: ['cloud://hao-weather-0y1l0.6861-hao-weather-0y1l0-1301154191/my-image.jpg'],
+			success: res=>{
+				console.log(res)
+			}
+		})
+	},
 
 
 })
